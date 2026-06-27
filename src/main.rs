670ea023a1524c9d6e22886826bcf23e7584bfea
caelondev <227796969+caelondev@users.noreply.github.com>
2026-06-27 22:35:33 +0800

    feat: add cli
    ok i lied, that wasnt my last commit lol

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
+use std::{
+    env,
+    io::{self, Write},
+};
+
 use bf_rust::BrainfuckRust;
 
 fn main() {
-    let src = r"#
-    ++++++++++[>+++++++>++++++++++>+++>+<<<<-]
->++.                    H
->+.                     e
-+++++++..+++.           llo
->++.                    (space)
-<<+++++++++++++++.      W
->.                      o
-+++.------.--------.    rld
->+.                     !
->.                      \n
-        #";
+    let args: Vec<String> = env::args().collect();
 
-    let mut interpreter = BrainfuckRust::new(src);
-    match interpreter.run() {
-        Ok(()) => {}
-        Err(e) => eprintln!("{e}"),
+    if args.len() > 1 {
+        // TODO: This is for reading from a file
+        // but im too lazy to implement it today
+        return;
+    }
+
+    loop {
+        let mut src = String::new();
+
+        print!("> ");
+        io::stdout().flush().expect("Cannot flush stdout");
+
+        io::stdin().read_line(&mut src).expect("Cannot read stdin");
+
+        let mut interpreter = BrainfuckRust::new(&src);
+        match interpreter.run() {
+            Ok(()) => {}
+            Err(e) => eprintln!("{e}"),
+        }
     }
 }
