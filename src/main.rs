bd64b50715a69ea489a58a5c2556827853fc646e
caelondev <227796969+caelondev@users.noreply.github.com>
2026-06-27 22:11:24 +0800

    feat: hello world
    probably my final commit

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,16 @@ use bf_rust::BrainfuckRust;
 
 fn main() {
     let src = r"#
+    ++++++++++[>+++++++>++++++++++>+++>+<<<<-]
+>++.                    H
+>+.                     e
++++++++..+++.           llo
+>++.                    (space)
+<<+++++++++++++++.      W
+>.                      o
++++.------.--------.    rld
+>+.                     !
+>.                      \n
         #";
 
     let mut interpreter = BrainfuckRust::new(src);
